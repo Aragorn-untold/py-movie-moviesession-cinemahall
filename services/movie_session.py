201da7fa41e8cd98f338f09fd15ceaf7851e6de1
Maksym <idytkoni@gmail.com>
2026-09-10 13:26:33 +0300

    done1

--- services/movie_session.py
+++ services/movie_session.py
@@ -11,8 +11,8 @@ def create_movie_session(
 ) -> None:
     MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=CinemaHall.objects.get(id=cinema_hall_id),
-        movie=Movie.objects.get(id=movie_id)
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id
     )
 
 
@@ -41,9 +41,9 @@ def update_movie_session(
         if show_time is not None:
             session.show_time = show_time
         if movie_id is not None:
-            session.movie = Movie.objects.get(id=movie_id)
+            session.movie_id = movie_id
         if cinema_hall_id is not None:
-            session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+            session.cinema_hall_id = cinema_hall_id
         session.save()
 
 
